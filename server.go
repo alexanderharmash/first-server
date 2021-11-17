dd506f58f0404498728d3aa90ae431d8de123fdf
Garmash Alexander <garmashalexander19@gmail.com>
2021-11-17 10:38:51 +0200

    work

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func form(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":		
+		 http.ServeFile(w, r, "index.html")
+	case "POST":
+		firstName := r.FormValue("fname");
+		lastName  := r.FormValue("lname");
+		fmt.Printf("firstName = %s\n", firstName)
+		fmt.Printf("lastName = %s\n", lastName)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", form)
+
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
